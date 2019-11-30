91b469160e2d46bfc162d611c801b69e1e99d5cc
Tejashri <teju.phalke@gmail.com>
2019-11-30 22:31:40 +0530

    First commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  product=[]; 
+  constructor(private service:ProductService) { }
+
+  ngOnInit() {
+    const observableResult=this.service.getProducts();
+    observableResult.subscribe((response)=>{
+      if(response['status']==='success')
+      {
+        this.product=response['data'] as any[];
+      }
+      else
+      {
+        console.log('error',response['error']);
+      }
+    })
+  }
+
+}
